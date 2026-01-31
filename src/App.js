acc5b404be6524c4286edad314d37077c2c445c2
Dylan Grieger <dylangrieger61@gmail.com>
2026-01-30 18:51:29 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styles from "./app.module.css";
 
 function App() {
+  const [mode, setMode] = useState("light");
+
+  const toggleMode = () => {
+    setMode(mode === "light" ? "dark" : "light");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`${styles.app} ${styles[mode]}`}>
+      <h1>My Profile</h1>
+
+      <button onClick={toggleMode}>
+        Switch to {mode === "light" ? "Dark" : "Light"} Mode
+      </button>
+
+      {/* Conditional Rendering */}
+      {mode === "light" ? (
+        <p>Light mode is active ☀️</p>
+      ) : (
+        <p>Dark mode is active 🌙</p>
+      )}
     </div>
   );
 }
